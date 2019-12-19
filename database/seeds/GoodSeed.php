3b9a7d9e326fde93c2b04a356624d3bfe84a5a7e
Fukalov <andrey.fukalov@payoola.com>
2019-12-19 16:02:15 +0400

    migrate for catalog

--- database/seeds/GoodSeed.php
+++ database/seeds/GoodSeed.php
@@ -146,6 +146,223 @@ class GoodSeed extends Seeder
                 'price' => '161.28',
                 'file' => '/images/hs160_2.jpg'
             ],
+            [
+                'group_id' => 4,
+                'name' => 'Шампунь Pantene в ассортименте',
+                'size' => '250 мл.',
+                'price' => '94.50',
+                'file' => '/images/pn1.jpg'
+            ],
+            [
+                'group_id' => 4,
+                'name' => 'Шампунь Pantene в ассортименте',
+                'size' => '400 мл.',
+                'price' => '131.81',
+                'file' => '/images/pn1.jpg'
+            ],
+            [
+                'group_id' => 4,
+                'name' => 'Шампунь Pantene',
+                'size' => '250 мл.',
+                'price' => '94.50',
+                'file' => '/images/pn2.jpg'
+            ],
+            [
+                'group_id' => 4,
+                'name' => 'Шампунь Pantene',
+                'size' => '400 мл.',
+                'price' => '131.81',
+                'file' => '/images/pn2.jpg'
+            ],
+            [
+                'group_id' => 4,
+                'name' => 'Бальзам Pantene',
+                'size' => '200 мл.',
+                'price' => '94.50',
+                'file' => '/images/pnb1.jpg'
+            ],
+            [
+                'group_id' => 4,
+                'name' => 'Бальзам Pantene',
+                'size' => '360 мл.',
+                'price' => '131.80',
+                'file' => '/images/pnb1.jpg'
+            ],
+            [
+                'group_id' => 4,
+                'name' => 'Шампунь 3 в 1',
+                'size' => '360 мл.',
+                'price' => '131.80',
+                'file' => '/images/pn3v1_1.jpg'
+            ],
+            [
+                'group_id' => 4,
+                'name' => 'Шампунь 3 в 1',
+                'size' => '360 мл.',
+                'price' => '131.80',
+                'file' => '/images/pn3v1_2.jpg'
+            ],
+            [
+                'group_id' => 5,
+                'name' => 'С/п Ariel для ручной стирки в ассортименте',
+                'size' => '450 гр.',
+                'price' => '57.96',
+                'file' => '/images/Ariel01.jpg'
+            ],
+            [
+                'group_id' => 5,
+                'name' => 'С/п Ariel Автомат в ассортименте',
+                'size' => '450 гр.',
+                'price' => '65.94',
+                'file' => '/images/Ariel02.jpg'
+            ],
+            [
+                'group_id' => 5,
+                'name' => 'С/п Tide для ручной стирки в ассортименте',
+                'size' => '400 гр.',
+                'price' => '36.20',
+                'file' => '/images/Tide01.jpg'
+            ],
+            [
+                'group_id' => 5,
+                'name' => 'Tide Автомат в ассортименте',
+                'size' => '450 гр.',
+                'price' => '52.50',
+                'file' => '/images/Tide02.jpg'
+            ],
+            [
+                'group_id' => 5,
+                'name' => 'С/п Tide Автомат в ассортименте',
+                'size' => '1.5 кг.',
+                'price' => '149.10',
+                'file' => '/images/Tide03.jpg'
+            ],
+            [
+                'group_id' => 5,
+                'name' => 'С/п Миф для ручной стирки в ассортименте',
+                'size' => '400 гр.',
+                'price' => '27.30',
+                'file' => '/images/Mif01.jpg'
+            ],
+            [
+                'group_id' => 5,
+                'name' => 'С/п Миф Автомат в ассортименте',
+                'size' => '400 гр.',
+                'price' => '31.49',
+                'file' => '/images/Mif02.jpg'
+            ],
+            [
+                'group_id' => 5,
+                'name' => 'С/п Миф для ручной стирки',
+                'size' => '1.8 кr.',
+                'price' => '116.76',
+                'file' => '/images/Mif03.jpg'
+            ],
+            [
+                'group_id' => 5,
+                'name' => 'С/п Миф Автомат в ассортименте',
+                'size' => '2 кr.',
+                'price' => '131.84',
+                'file' => '/images/Mif04.jpg'
+            ],
+            [
+                'group_id' => 6,
+                'name' => 'Always Ultra',
+                'size' => '10 шт.',
+                'price' => '67.79',
+                'file' => '/images/aw01.jpg'
+            ],
+            [
+                'group_id' => 6,
+                'name' => 'Always Night',
+                'size' => '6 шт.',
+                'price' => '67.79',
+                'file' => '/images/aw02.jpg'
+            ],
+            [
+                'group_id' => 6,
+                'name' => 'Always Ultra Super',
+                'size' => '8 шт.',
+                'price' => '67.79',
+                'file' => '/images/aw03.jpg'
+            ],
+            [
+                'group_id' => 6,
+                'name' => 'Always Ultra Normal',
+                'size' => '10 шт.',
+                'price' => '67.79',
+                'file' => '/images/aw04.jpg'
+            ],
+            [
+                'group_id' => 6,
+                'name' => 'Always Ultra Night',
+                'size' => '7 шт.',
+                'price' => '67.79',
+                'file' => '/images/aw05.jpg'
+            ],
+            [
+                'group_id' => 6,
+                'name' => 'Ежедневные гигиенические прокладки Descreet в ассортименте',
+                'size' => '100 шт.',
+                'price' => '160.10',
+                'file' => '/images/ds01.jpg'
+            ],
+            [
+                'group_id' => 6,
+                'name' => 'Ежедневные гигиенические прокладки Descreet в ассортименте',
+                'size' => '60 шт.',
+                'price' => '103.58',
+                'file' => '/images/ds02.jpg'
+            ],
+            [
+                'group_id' => 6,
+                'name' => 'Ежедневные гигиенические прокладки Descreet в ассортименте',
+                'size' => '20 шт.',
+                'price' => '37.07',
+                'file' => '/images/ds03.jpg'
+            ],
+            [
+                'group_id' => 6,
+                'name' => 'Naturella Ultpa Maxi',
+                'size' => '8 шт.',
+                'price' => '60.58',
+                'file' => '/images/nt01.jpg'
+            ],
+            [
+                'group_id' => 6,
+                'name' => 'Naturella Ultra Normal',
+                'size' => '10 шт.',
+                'price' => '60.58',
+                'file' => '/images/nt02.jpg'
+            ],
+            [
+                'group_id' => 6,
+                'name' => 'Naturella Ultra Normal',
+                'size' => '20 шт.',
+                'price' => '350',
+                'file' => '/images/nt06.jpg'
+            ],
+            [
+                'group_id' => 6,
+                'name' => 'Ежедневные гигиенические прокладки Naturella в ассортименте',
+                'size' => '52 шт.',
+                'price' => '122.40',
+                'file' => '/images/nt03.jpg'
+            ],
+            [
+                'group_id' => 6,
+                'name' => 'Ежедневные гигиенические прокладки Naturella в ассортименте',
+                'size' => '100 шт.',
+                'price' => '350',
+                'file' => '/images/nt05.jpg'
+            ],
+            [
+                'group_id' => 6,
+                'name' => 'Ежедневные гигиенические прокладки Naturella в ассортименте',
+                'size' => '20 шт.',
+                'price' => '50.12',
+                'file' => '/images/nt04.jpg'
+            ],
         ];
 
         DB::table('goods')->insert($data);
